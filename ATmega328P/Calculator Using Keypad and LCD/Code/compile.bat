d6d0033f6d2dde38c2194d16f808d8daf01bbe98
Omar Elsobkey <omarelsobkey9009@gmail.com>
2021-03-25 22:17:23 +0200

    modify calculator.c name in compile.bat and re-run it

--- ATmega328P/Calculator Using Keypad and LCD/Code/compile.bat
+++ ATmega328P/Calculator Using Keypad and LCD/Code/compile.bat
@@ -1,6 +1,6 @@
 @echo off
 
-avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o out.o "calculator (v2).c"
+avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o out.o "calculator.c"
 avr-gcc -mmcu=atmega328p out.o -o out
 avr-objcopy -O ihex -R .eeprom out out.hex
 
